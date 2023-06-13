9d3827459c9231388ecf4f4c6c0a119142966497
ManuelPeva <127805450+ManuelPeva@users.noreply.github.com>
2023-06-12 21:34:40 -0600

    Incorporación de sonido de baliza

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 radio.setGroup(1)
-radio.setTransmitPower(1)
+radio.setTransmitPower(7)
 let id = 1
 basic.showNumber(id)
 basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     radio.sendNumber(id)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
     basic.pause(200)
 })
